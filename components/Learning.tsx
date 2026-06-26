dd295009f13fc6c0605c1b30284a9d6724aac98b
amu815 <suemoto.amu.815@gmail.com>
2026-06-26 10:17:31 +0900

    Improve Rust logo contrast

--- components/Learning.tsx
+++ components/Learning.tsx
@@ -8,7 +8,9 @@ export function Learning() {
         <img src={`${DEV}/go/go-original-wordmark.svg`} alt="Go" width={56} height={56} />
       </a>
       <a href="https://www.rust-lang.org/" target="_blank" rel="noreferrer">
-        <img src={`${DEV}/rust/rust-original.svg`} alt="Rust" width={56} height={56} />
+        <span className="inline-flex h-14 w-14 items-center justify-center rounded-md bg-white/90 p-1.5 ring-1 ring-white/70">
+          <img src={`${DEV}/rust/rust-original.svg`} alt="Rust" width={44} height={44} />
+        </span>
       </a>
     </div>
   );
